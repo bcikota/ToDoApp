d9afbfd0a3e7139d054a2a787d6eaaac62f2ceb1
bcikota <bcikota@gmail.com>
2020-12-15 11:05:38 +0100

    Pass and view data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [lists, setLists] = useState([]);
+
+  axios({
+    method: 'get',
+    url: 'https://shrouded-journey-60588.herokuapp.com/'
+  })
+    .then((response) => {
+      setLists(()=>{
+        return [...response.data];
+      });
+    });
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>TodoApp</h1>
+      <ul>
+        {lists.map((list, index) => {
+          return <li key={index}>{list.name}</li>
+        })}
+      </ul>
+
     </div>
   );
 }
