425d6b5cb665b79072c020cd821994a9747cb262
bcikota <bcikota@gmail.com>
2020-12-18 16:56:11 +0100

    Add CRUD for /listsUrl

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  //---------Add CRUD for '/lists'-------
-  const [lists, setLists] = useState([]);
-  const [listName, setlistName] = useState('');
+
+  const [lists, setLists] = useState([]); //read lists
+  const [listName, setlistName] = useState(''); //create & edit list
+  const [renderUrl, setRenderUrl] = useState('');
+  const [clickedList, setClickedList] = useState('');
+  const [listItems, setListItems] = useState([]); //read list items
+  const [editUrl, setEditUrl] = useState('');
+  const [editListActive, setEditListActive] = useState(false); //if editing input is active
+  const [deleteUrl, setDeleteUrl] = useState('');
+  const [isListClicked, setIsListClicked] = useState(false);
+  const [itemName, setItemName] = useState('');
+  const [postItemUrl, setPostItemUrl] = useState('');
+  const [editItemUrl, setEditItemUrl] = useState('');
+  const [deleteItemUrl, setDeleteItemUrl] = useState('');
+  const [editItemActive, setEditItemActive] = useState(false);
+  const [temporaryListName, setTemporaryListName] = useState('');
+
 
   axios.defaults.baseURL = 'https://shrouded-journey-60588.herokuapp.com/';
 
   useEffect(() => {
+    //read lists
     axios.get('/')
       .then((response) => {
         setLists(() => {
@@ -18,28 +33,51 @@ function App() {
   }, []);
 
   function handleChange(e) {
-    let targetValue = e.target.value;
-    setlistName(targetValue);
+    setlistName(e.target.value);
   }
 
-  function handleListSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
+    //create & edit list
+    if (!editListActive && listName) {
+      axios.post(
+        '/lists',
+        {
+          name: listName
+        }
+      )
+        .then((response) => {
+          setLists(() => {
+            return [...response.data];
+          });
+          setlistName('');
+          setIsListClicked(false);
+        });
+    } else if(editUrl){
 
-    axios.post(
-      '/lists',
-      {
-        name: listName
-      }
-    )
-    .then((response) => {
-      setLists(() => {
-        return [...response.data];
-      });
-      setlistName('');
-    });
+      axios.patch('lists/' + editUrl,
+        {
+          name: listName
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then((response) => {
+          setLists(() => {
+            return [...response.data];
+          });
+          setEditListActive(false);
+          setlistName('');
+          setIsListClicked(false);
+        });
+
+    }
+
+
   }
 
   function handleDeleteAllLists() {
+    //delete all lists
     axios.delete('/lists')
       .catch(function (error) {
         console.log(error);
@@ -50,35 +88,124 @@ function App() {
         });
         setClickedList('');
         setListItems([]);
+        setIsListClicked(false);
       });
   }
-  //----END-----Add CRUD for '/lists'-------
-
-
-  const [listUrl, setListUrl] = useState('');
 
 
   function handleListClick(e) {
-    let listName = escape(e.target.textContent);
-    setListUrl(listName);
-    // setIsListClicked((prevValue) => !prevValue);
+    let currListName = escape(e.target.textContent);
+    setRenderUrl(currListName);
+    setIsListClicked(true);
   }
 
-  const [clickedList, setClickedList] = useState('');
-  const [listItems, setListItems] = useState([]);
-  
+  useEffect(() => {
+    //read list items
+    axios.get('lists/' + renderUrl)
+      .then((res) => {
+        setClickedList(res.data.name);
+        if (res.data.items) {
+          setListItems([...res.data.items]);
+        }
+      });
+  }, [renderUrl]);
 
-  
 
-    useEffect(() => {
-      axios.get('lists/' + listUrl)
-        .then((res) => {
-          setClickedList(res.data.name);
-      if (res.data.items) {
-        setListItems([...res.data.items])
-      }
+  function handleEditList(e) {
+    setEditListActive((prevValue) => !prevValue);
+    handleChange(e);
+    setEditUrl(escape(e.target.value));
+  }
+
+
+
+  function handleDeleteList(e) {
+    setDeleteUrl(escape(e.target.value));
+  }
+
+  useEffect(() => {
+    //delete specific list
+    if (deleteUrl !== '') {
+      axios.delete('lists/' + deleteUrl)
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then((response) => {
+          setLists(() => {
+            return [...response.data];
+          });
+          setIsListClicked(false);
         });
-    }, [listUrl]);
+    }
+  }, [deleteUrl]);
+
+
+
+  function handleItemChange(e) {
+    setItemName(e.target.value);
+    setPostItemUrl(escape(clickedList));
+  }
+
+  function handleItemSubmit(e) {
+    e.preventDefault();
+
+    if (postItemUrl && !editItemActive) {
+      axios.post('lists/' + postItemUrl,
+        {
+          name: itemName
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then((response) => {
+          setListItems([...response.data.items]);
+          setItemName('');
+          setPostItemUrl('');
+        });
+    } else if(editItemUrl) {
+   
+      axios.patch('lists/' + editItemUrl,
+      {
+        name: itemName
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then((response) => {
+        setListItems([...response.data[0].items]);
+        setClickedList(temporaryListName);
+        setEditItemUrl('');
+        setEditItemActive(false);
+        setItemName('');
+      });
+    }
+  }
+
+  function handleEditItem(e) {
+    handleItemChange(e);
+    setEditItemUrl(escape(clickedList+"/"+e.target.value));
+    setEditItemActive(true);
+    setTemporaryListName(clickedList);
+  }
+
+
+
+  function handleDeleteItem(e) {
+    setDeleteItemUrl(escape(clickedList+"/"+e.target.value));
+  }
+
+  useEffect(() => {
+    //delete specific Item
+    if (deleteItemUrl !== '') {
+      axios.delete('lists/' + deleteItemUrl)
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then((response) => {
+          setListItems([...response.data.items]);
+        });
+    }
+  }, [deleteItemUrl]);
 
   return (
     <div className="App">
@@ -86,20 +213,37 @@ function App() {
 
       <ul>
         {lists.map((list, index) => {
-          return <li key={index} onClick={handleListClick} >{list.name}</li>
+          return <li key={index} style={{ paddingBottom: '1rem' }}>
+            <button onClick={handleListClick} style={{ marginRight: '1rem', background: 'none', border: 'none', textDecoration: 'underline', cursor: 'pointer' }}>{list.name}</button>
+
+            {list.name !== 'default list' &&
+              <button value={list.name} onClick={handleEditList}>edit</button>}
+            {list.name !== 'default list' &&
+              <button value={list.name} onClick={handleDeleteList} >delete</button>}
+
+          </li>
         })}
       </ul>
-      <form onSubmit={handleListSubmit}>
-        <input type="text" name="name" onChange={handleChange} value={listName} />
-        <button type="submit">add new list</button>
+      <form onSubmit={handleSubmit} style={{ marginBottom: '1rem' }}>
+        <input type="text" name="name" onChange={handleChange} value={listName} placeholder="add new list" />
+        <button type="submit">ok</button>
       </form>
       <button onClick={handleDeleteAllLists}> delete all lists</button>
-      <div>
+      <div style={isListClicked === false ? { display: 'none' } : { display: 'block' }}>
         <h2>{clickedList}</h2>
         <ul>
           {listItems.map((item, index) => {
-            return <li key={index} >{item.name}</li>
+
+            return <li key={index} style={{ marginBottom: '1rem' }}> <span style={{ marginRight: '1rem' }}>{item.name}</span>
+              <button value={item.name} onClick={handleEditItem}>edit</button>
+              <button value={item.name} onClick={handleDeleteItem} >delete</button>
+            </li>
+
           })}
+          <form onSubmit={handleItemSubmit}>
+            <input type="text" onChange={handleItemChange} value={itemName} placeholder="add new item" />
+            <button type="submit">+</button>
+          </form>
         </ul>
       </div>
     </div>
